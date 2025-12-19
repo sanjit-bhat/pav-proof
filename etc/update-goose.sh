b1ff10001b6d511248cbf49b8a6d9c0ec8ed32a0
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-12-19 13:09:02 -0600

    add goosed outs

--- etc/update-goose.sh
+++ etc/update-goose.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-go tool goose -out code -dir ../pav
+go tool goose -out code -dir ../pav \
+    ./advrpc ./alicebob ./auditor ./client ./cryptoffi ./cryptoutil \
+    ./hashchain ./ktcore ./merkle ./netffi ./safemarshal ./server
 go tool proofgen -out generatedproof -configdir code -dir ../pav \
     ./advrpc ./alicebob ./auditor ./client ./cryptoffi ./cryptoutil \
     ./hashchain ./ktcore ./merkle ./netffi ./safemarshal ./server
