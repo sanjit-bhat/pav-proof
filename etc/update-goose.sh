fed696c72f52c76d3d0fef0a31dae82cbdba2fe7
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-12-22 12:04:00 -0500

    make update-goose more robust

--- etc/update-goose.sh
+++ etc/update-goose.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+set -euo pipefail  # Exit on error, undefined vars, pipe failures
 
-go tool goose -out code -dir ../pav \
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <pav dir>" >&2
+    exit 1
+fi
+
+readonly PAV="$1"
+
+go tool goose -out code -dir "$PAV" \
     ./advrpc ./alicebob ./auditor ./client ./cryptoffi ./cryptoutil \
     ./hashchain ./ktcore ./merkle ./netffi ./safemarshal ./server
-go tool proofgen -out generatedproof -configdir code -dir ../pav \
+go tool proofgen -out generatedproof -configdir code -dir "$PAV" \
     ./advrpc ./alicebob ./auditor ./client ./cryptoffi ./cryptoutil \
     ./hashchain ./ktcore ./merkle ./netffi ./safemarshal ./server
